acf2a8ed91dc8f11ea7b9f99714485b66d9a080e
MAURICE Nicolas <nicolas.maurice@inria.fr>
2024-07-12 17:24:36 +0200

    added the git branch and hash to the logs, fixed the snakemake logs

--- run.sh
+++ run.sh
@@ -13,12 +13,16 @@ if [ -d "$output_directory/logs/$analysis_name" ]; then
 fi
 
 mkdir -p $output_directory/logs/$analysis_name
-cp config/config.yaml $output_directory/logs/$analysis_name/config.yaml
-export SBATCH_DEFAULTS=" --output=$output_directory/logs/$analysis_name/"
 
+cp config/config.yaml $output_directory/logs/$analysis_name/config.yaml
+
+echo "Branch: $(git rev-parse --abbrev-ref HEAD)" > $output_directory/logs/$analysis_name/git_info.txt
+echo "Latest Commit: $(git rev-parse HEAD)" >> $output_directory/logs/$analysis_name/git_info.txt
 
 snakemake all --cores all  --slurm --jobs 10 \
     --use-conda --rerun-triggers mtime \
     --configfile $output_directory/logs/$analysis_name/config.yaml \
-    --default-resources mem_mb=5000 mem_mb_per_cpu=None runtime=2*60 
-    
+    --default-resources mem_mb=5000 mem_mb_per_cpu=None runtime=2*60  \
+
+mv .snakemake/log/* $output_directory/logs/$analysis_name/
+mv .snakemake/slurm_logs/* $output_directory/logs/$analysis_name/
